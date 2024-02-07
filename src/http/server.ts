f606a565ac389d789192fb93f4d8612150d6ffd6
Thiago Domingues <thenrique2012@gmail.com>
2024-02-07 13:33:29 -0300

    feat: create poll options when poll is created

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,7 @@
 import fastify from "fastify";
-import { PrismaClient } from '@prisma/client';
-import { z } from 'zod';
 import { createPoll } from "../routes/create-poll";
 
 const app = fastify();
-const prisma = new PrismaClient();
 
 app.register(createPoll);
 
